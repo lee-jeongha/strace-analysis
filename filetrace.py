8bcd8e7b2977a4c41e9022cb80d3755ad1a0fb1b
JeongHa Lee <l03jeongha@gmail.com>
2022-05-03 12:11:49 +0900

    rearrange codes

--- filetrace.py
+++ filetrace.py
@@ -35,27 +35,32 @@ wf = open(args.output, 'w')
 fio_info = dict()   # {'pid, fd': [filename, offset]}
 ppid = dict()   # {'pid': 'ppid'}
 
+# column
+C_time=0
+C_pid=1
+C_op=2    # operation
+C_cpid=3    # child process pid
+C_fd=4
+C_offset=5
+C_length=6
+C_mem=7     # memory address
+C_flname=8    # filename
+C_ino=9   # inode
+
+
+# get parent pid
+def getppid(pid):
+    try:
+        p = ppid[pid]
+        return p
+    except KeyError:
+        return pid
+
 for line in rlines:
     line = line.strip("\n") # remove '\n'
 
     # separate the syscall log by comma
     s = line.split(',')
-    
-    # column
-    C_time=0
-    C_pid=1
-    C_op=2    # operation
-    C_cpid=3    # child process pid
-    C_fd=4
-    C_offset=5
-    C_length=6
-    C_mem=7     # memory address
-    C_flname=8    # filename
-    C_ino=9   # inode
-    
-    ###open -> file이름으로 inode찾기 -> {pid,fd:[inode,offset]}
-    ###lseek -> pid,fd로 offset찾기&inode매칭 -> ppid,fd로 inode매칭(offset은???)...하지 말고 fork 나오면 dict도 fork하자.
-    ###      -> 실행중인 프로세스를 붙여서 했을 경우, ppid,fd도 inode 안 나올 수 있음 -> 문자열 inode 임의로 만들자. (이렇게 하면 재참조는 못 봄)
 
     #---
     if s[C_op]=='open' or s[C_op]=='openat' or s[C_op]=='creat':
@@ -83,47 +88,68 @@ for line in rlines:
         #print(ppid)
 
     elif s[C_op]=='read' or s[C_op]=='write':
+        p_pid = getppid(s[C_pid])
+        # 1. fd==0:stdin, fd==1:stdout, fd==2:stderr
+        if s[C_fd]=='0' or s[C_fd]=='1' or s[C_fd]=='2':
+            wlines = s[C_time] + "," + s[C_pid] + "," + p_pid + "," + s[C_op] + "," + s[C_fd] + "," + "0," + s[C_length]
+            wf.write(wlines + "\n")
+            continue
+        # 2. use pid
         try:
             file_info = fio_info.pop(s[C_pid]+","+s[C_fd])    # s[1]:pid, s[4]:fd
             offset = int(file_info[1])
             #---
-            wlines = s[C_time] + "," + s[C_pid] + ",," + s[C_op] + "," + s[C_fd] + "," + str(offset) + "," + s[C_length] + "," + file_info[0]
+            wlines = s[C_time] + "," + s[C_pid] + "," + p_pid + "," + s[C_op] + "," + s[C_fd] + "," + str(offset) + "," + s[C_length] + "," + file_info[0]
             wf.write(wlines + "\n")
             #---
-            file_info[1] = offset + int(s[C_length])    # update offset
+            file_info[1] = offset + int(s[C_length])	# update offset
             fio_info[s[C_pid]+","+s[C_fd]] = file_info
         except KeyError:
-            # 1. fd==0:stdin, fd==1:stdout, fd==2:stderr
-            if s[C_fd]=='0' or s[C_fd]=='1' or s[C_fd]=='2':
-                wlines = s[C_time] + "," + s[C_pid] + ",," + s[C_op] + "," + s[C_fd] + "," + "0," + s[C_length]
+            # 3. try to use ppid
+            try:	# (p_pid != s[C_pid])
+                file_info = fio_info.pop(p_pid+","+s[C_fd])    # s[1]:pid, s[4]:fd
+                offset = int(file_info[1])
+                #---
+                wlines = s[C_time] + "," + s[C_pid] + "," + p_pid + "," + s[C_op] + "," + s[C_fd] + "," + str(offset) + "," + s[C_length] + "," + file_info[0]
                 wf.write(wlines + "\n")
-            
-            else:
-                try:
-                    # 2. parent pid
-                    p_pid = ppid.pop(s[C_pid])
-                    #---
-                    file_info = fio_info.pop(p_pid+","+s[C_fd])
-                    offset = int(file_info[1])
-                    #---
-                    wlines = s[C_time] + "," + s[C_pid] + "," + p_pid + "," + s[C_op] + "," + s[C_fd] + "," + str(offset) + "," + s[C_length] + "," + file_info[0]
-                    wf.write(wlines + "\n")
-                    #---
-                    file_info[1] = offset + int(s[C_length])	# update offset
-                    file_info[p_pid+","+s[C_fd]] = file_info
-                except KeyError:
-                    # 3. error case
-                    wlines = s[C_time] + "," + s[C_pid] + ",," + s[C_op] + "," + s[C_fd] + "," + "error," + s[C_length]
-                    wf.write(wlines + "\n")
+                #---
+                file_info[1] = offset + int(s[C_length])	# update offset
+                fio_info[s[C_pid]+","+s[C_fd]] = file_info
+            # 4. error case
+            except KeyError:
+                wlines = s[C_time] + "," + s[C_pid] + "," + p_pid + "," + s[C_op] + "," + s[C_fd] + "," + "error," + s[C_length]
+                wf.write(wlines + "\n")
 
     elif s[C_op]=='pread64' or s[C_op]=='pwrite64':
-        file_info = fio_info.pop(s[C_pid]+","+s[C_fd])    # s[1]:pid, s[4]:fd
-        #---
-        wlines = s[C_time] + "," + s[C_pid] + ",," + s[C_op] + "," + s[C_fd] + "," + s[C_offset] + "," + s[C_length] + "," + file_info[0]
-        wf.write(wlines + "\n")
-        #---
-        file_info[1] = int(s[C_offset]) + int(s[C_length])	# update offset
-        fio_info[s[C_pid]+","+s[C_fd]] = file_info
+        p_pid = getppid(s[C_pid])
+        # 1. fd==0:stdin, fd==1:stdout, fd==2:stderr
+        if s[C_fd]=='0' or s[C_fd]=='1' or s[C_fd]=='2':
+            wlines = s[C_time] + "," + s[C_pid] + "," + p_pid + "," + s[C_op] + "," + s[C_fd] + "," + "0," + s[C_length]
+            wf.write(wlines + "\n")
+            continue
+        # 2. use pid
+        try:
+            file_info = fio_info.pop(s[C_pid]+","+s[C_fd])    # s[1]:pid, s[4]:fd
+            #---
+            wlines = s[C_time] + "," + s[C_pid] + "," + p_pid + "," + s[C_op] + "," + s[C_fd] + "," + s[C_offset] + "," + s[C_length] + "," + file_info[0]
+            wf.write(wlines + "\n")
+            #---
+            file_info[1] = int(s[C_offset]) + int(s[C_length])	# update offset
+            fio_info[s[C_pid]+","+s[C_fd]] = file_info
+        except KeyError:
+            # 3. try to use ppid
+            try:	# (p_pid != s[C_pid])
+                file_info = fio_info.pop(p_pid+","+s[C_fd])    # s[1]:pid, s[4]:fd
+                #---
+                wlines = s[C_time] + "," + s[C_pid] + "," + p_pid + "," + s[C_op] + "," + s[C_fd] + "," + s[C_offset] + "," + s[C_length] + "," + file_info[0]
+                wf.write(wlines + "\n")
+                #---
+                file_info[1] = int(s[C_offset]) + int(s[C_length])	# update offset
+                fio_info[s[C_pid]+","+s[C_fd]] = file_info
+            # 4. error case
+            except KeyError:
+                wlines = s[C_time] + "," + s[C_pid] + "," + p_pid + "," + s[C_op] + "," + s[C_fd] + "," + "error," + s[C_length]
+                wf.write(wlines + "\n")
 
 rf.close()
 wf.close()
