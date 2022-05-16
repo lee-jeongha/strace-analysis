f8a358950df5e684b2c7e3d34506097e279a806d
JeongHa Lee <l03jeongha@gmail.com>
2022-05-16 20:07:47 +0900

    edit gnuplot code

--- run.sh
+++ run.sh
@@ -12,13 +12,15 @@ python3 filetrace.py $target/0parse.csv $target/1-2fileio.csv $target/1-1inode.c
 python3 filerefblk.py $target/1-2fileio.csv $target/2fileblk.csv
 
 # memory access per logical time
+columns=(7 8)
 gnuplot << EOF
   set datafile separator ','
   set title "reference block"
   set xlabel "time"
   set ylabel "block number"
-  set term png
+  set term png size 1500,1100
   set output "$target/2fileblk.png"
-  plot "$target/2fileblk.csv" using 2:5
+  plot "$target/2fileblk.csv" using 2:7 lt rgb "blue" title "read",\
+"$target/2fileblk.csv" using 2:8 lt rgb "red" title "write"
 EOF
 
