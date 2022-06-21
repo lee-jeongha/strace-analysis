e825d6a58d81c2bb03d2ec8766b2779fe080f918
JeongHa Lee <l03jeongha@gmail.com>
2022-06-21 19:35:13 +0900

    Combine two log files into one

--- fileio/plot/1refcountperblock.py
+++ fileio/plot/1refcountperblock.py
@@ -2,6 +2,7 @@ import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 # add parser
 parser = argparse.ArgumentParser(description="plot reference count per each block")
@@ -20,7 +21,7 @@ def save_csv(df, filename="output.csv", index=0):
     #if not os.path.exists(path):
     target_dir = filename.rfind('/')
     path = filename[:target_dir]
-    os.mkdir(path)
+    os.makedirs(path)
     #---
     if index==0:
       df.to_csv(filename, index=True, header=True, mode='w') # encoding='utf-8-sig'
@@ -51,19 +52,15 @@ for i in range(len(blkdf)):
   df = address_ref(blkdf[i], concat=False)
   df1 = pd.concat([df1, df])
 
+#group by type(read or write)
 df1 = address_ref(df1, concat=True)
 
-#only read
-df1['readcount'] = df1['count']
-df1.loc[(df1.operation=='write'), 'readcount'] = np.NaN
-#only write
-df1['writecount'] = df1['count']
-df1.loc[(df1.operation!='write'), 'writecount'] = np.NaN
-#read and write
+#both read and write
 df1_rw = df1.groupby(by=['blocknum'], as_index=False).sum()
+df1_rw['operation'] = 'read&write'
 
+df1 = pd.concat([df1, df1_rw], sort=True)
 save_csv(df1, args.output, 0)
-save_csv(df1_rw, args.output[:-4]+'_rw.csv', 0)
 
 '''
 **blkdf1 graph**
@@ -71,21 +68,21 @@ save_csv(df1_rw, args.output[:-4]+'_rw.csv', 0)
 '''
 
 blkdf1 = pd.read_csv(args.output, sep=',', header=0, index_col=0, on_bad_lines='skip')
-blkdf1_rw = pd.read_csv(args.output[:-4]+'_rw.csv', sep=',', header=0, index_col=0, on_bad_lines='skip')
 
 #plt.style.use('default')
 plt.rcParams['figure.figsize'] = (24, 20)
 plt.rcParams['font.size'] = 25
 
 # scatter
-x = blkdf1['blocknum']
-y1 = blkdf1['readcount']
-y2 = blkdf1['writecount']
-print(x.min(), x.max())
+x1 = blkdf1['blocknum'][(blkdf1['operation']=='read')]
+x2 = blkdf1['blocknum'][(blkdf1['operation']=='write')]
+y1 = blkdf1['count'][(blkdf1['operation']=='read')]
+y2 = blkdf1['count'][(blkdf1['operation']=='write')]
+print(x1.max(), x2.max())
 print(y1.max(), y2.max())
 
-plt.scatter(x, y1, color='blue', label='read') #aquamarine, s=5
-plt.scatter(x, y2, color='red', label='write') #salmon, s=5
+plt.scatter(x1, y1, color='blue', label='read') #aquamarine, s=5
+plt.scatter(x2, y2, color='red', label='write') #salmon, s=5
 
 # legend
 plt.xlabel('unique block number')
