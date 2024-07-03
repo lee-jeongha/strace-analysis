08e9864ac4ba0308c232e1b9754b6e5b8d152281
JeongHa Lee <l03jeongha@gmail.com>
2024-07-03 12:16:16 +0900

    update `plot_frame()`

--- fileio/plot/plot_graph.py
+++ fileio/plot/plot_graph.py
@@ -3,24 +3,26 @@ import matplotlib.pyplot as plt
 def plot_frame(subplot_matrix : tuple = (1, 1), subplot_figsize : tuple = (7, 7), title = '', xlabel = '', ylabel = '', font_size = 20, log_scale = False, share_xaxis : str or bool = True, share_yaxis : str or bool = True):
     subplot_rows = subplot_matrix[0]
     subplot_cols = subplot_matrix[1]
-    subplot_row_size = subplot_matrix[1] * subplot_figsize[0]
-    subplot_col_size = subplot_matrix[0] * subplot_figsize[1]
+    subplot_width = subplot_matrix[1] * subplot_figsize[0]
+    subplot_height = subplot_matrix[0] * subplot_figsize[1]
+
+    single_frame = False if subplot_rows * subplot_cols > 1 else True
 
     font_size = font_size
     plt.rc('font', size=font_size)
 
-    fig, ax = plt.subplots(subplot_rows, subplot_cols, figsize=(subplot_row_size, subplot_col_size), constrained_layout=True, sharex=share_xaxis, sharey=share_yaxis)
+    fig, ax = plt.subplots(subplot_rows, subplot_cols, figsize=(subplot_width, subplot_height), constrained_layout=True, sharex=share_xaxis, sharey=share_yaxis)
 
     if title != '':
-        plt.suptitle(title, fontsize = font_size + 10)
+        plt.suptitle(title, fontsize = font_size * 1.5)
 
     if log_scale:
         plt.xscale('log')
         plt.yscale('log')
 
     if xlabel != '':
-        fig.supxlabel(xlabel, fontsize = font_size + 5)
+        ax.set_xlabel(xlabel, fontsize = font_size * 1.25) if single_frame else fig.supxlabel(xlabel, fontsize = font_size * 1.25)
     if ylabel != '':
-        fig.supylabel(ylabel, fontsize = font_size + 5)
+        ax.set_ylabel(ylabel, fontsize = font_size * 1.25) if single_frame else fig.supylabel(ylabel, fontsize = font_size * 1.25)
 
     return fig, ax
