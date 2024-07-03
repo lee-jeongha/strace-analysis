83f55a3a684871ad3d8cd4525bb4585458bb0406
JeongHa Lee <l03jeongha@gmail.com>
2024-07-03 12:35:47 +0900

    handling an exceptional case

--- fileio/plot/load_and_save.py
+++ fileio/plot/load_and_save.py
@@ -2,25 +2,38 @@ import os
 import json
 
 def save_csv(df, filename, index=0):
-    path = filename[:filename.rfind('/')]
+    try:
+        if index==0:
+            df.to_csv(filename, index=True, header=True, mode='w') # encoding='utf-8-sig'
+        else: #append mode
+            df.to_csv(filename, index=True, header=False, mode='a') # encoding='utf-8-sig'
 
-    if not os.path.exists(path):    # FileNotFoundError: [Errno2] No such file or directory: '~'
-        os.makedirs(path)
+    except FileNotFoundError:    # FileNotFoundError: [Errno2] No such file or directory: '~'
+        path = filename[:filename.rfind('/')]
 
-    if index==0:
-        df.to_csv(filename, index=True, header=True, mode='w') # encoding='utf-8-sig'
-    else: #append mode
-        df.to_csv(filename, index=True, header=False, mode='a') # encoding='utf-8-sig'
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        if index==0:
+            df.to_csv(filename, index=True, header=True, mode='w') # encoding='utf-8-sig'
+        else: #append mode
+            df.to_csv(filename, index=True, header=False, mode='a') # encoding='utf-8-sig'
 
 def save_json(savings, filename):
-    path = filename[:filename.rfind('/')]
+    try:
+        with open(filename, 'w', encoding='utf-8') as f:
+            # indent=2 is not needed but makes the file human-readable
+            json.dump(savings, f, indent=2)
 
-    if not os.path.exists(path):    # FileNotFoundError: [Errno2] No such file or directory: '~'
-        os.makedirs(path)
+    except FileNotFoundError:    # FileNotFoundError: [Errno2] No such file or directory: '~'
+        path = filename[:filename.rfind('/')]
 
-    with open(filename, 'w', encoding='utf-8') as f:
-        # indent=2 is not needed but makes the file human-readable
-        json.dump(savings, f, indent=2)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            # indent=2 is not needed but makes the file human-readable
+            json.dump(savings, f, indent=2)
 
 def load_json(saving_list, filename):
     with open(filename, 'r') as f:
